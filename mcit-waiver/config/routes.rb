271f00af5735c27e629014f6aad556eb292962ab
Britt Binler <brittany.binler@gmail.com>
2015-11-30 09:44:20 -0500

    updated index page

--- mcit-waiver/config/routes.rb
+++ mcit-waiver/config/routes.rb
@@ -53,7 +53,7 @@ McitWaiver::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'waivers#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
