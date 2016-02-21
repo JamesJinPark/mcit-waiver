de055771e1f45c38656a14875b73b570926ed5a9
JamesJinPark <jamesjinpark@gmail.com>
2016-02-21 17:39:25 -0500

    Modified settings for secret token

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ McitWaiver::Application.configure do
     :address => "smtp.gmail.com",
     :port => "587",
     :domain => "gmail.com",
-    :user_name => "McitWaiver@gmail.com",
-    :password => "McitWaiver1234567890",
+    :user_name => ENV["GMAIL"]||"McitWaiver@gmail.com",
+    :password => ENV["PASSWORD"]||"McitWaiver1234567890",
     :authentication => "plain",
     :enable_starttls_auto => true
   }
--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => "587",
 	:domain => "gmail.com",
-	:user_name => "McitWaiver@gmail.com",
-	:password => "McitWaiver1234567890",
+	:user_name => ENV["GMAIL"]||"McitWaiver@gmail.com",
+	:password => ENV["PASSWORD"]||"McitWaiver1234567890",
 	:authentication => "plain",
 	:enable_starttls_auto => true
 }
