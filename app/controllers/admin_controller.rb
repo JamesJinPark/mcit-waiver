225cb6a516e59fa385dd681d4f92f856afc53e2f
JamesJinPark <jamesjinpark@gmail.com>
2016-02-22 17:04:35 -0500

    Fixed bug with instructor and admin being able to sign in at same time

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,12 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
   
   def home
+
+  	#signs out all instructors when administrator logs in
+  	for user in User.all 
+  		sign_out user
+	end
+
   end
+
 end
